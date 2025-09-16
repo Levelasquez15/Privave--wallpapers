958dfce49225d73d97ac7597f00f5b39d8dd0239
Levelasquez15 <lewis.velasquezwatts@unicolombo.edu.co>
2025-09-16 02:30:28 -0500

    instalaciones y creacion de todo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,27 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
+  
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./app/pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./app/pages/register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./app/pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'update-user-info',
+    loadChildren: () => import('./app/pages/update-user-info/update-user-info.module').then( m => m.UpdateUserInfoPageModule)
   },
 ];
 
